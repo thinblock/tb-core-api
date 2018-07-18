54b126313f95f02695f5563f63d7b0af2fcb163d
Hamza Baig <hamzabaig18@gmail.com>
2018-07-18 18:38:10 +0500

    fixes linting errors

--- app/middlewares/restifyAsyncAwait.ts
+++ app/middlewares/restifyAsyncAwait.ts
@@ -2,7 +2,7 @@ import { Server } from 'restify';
 import * as restifyPromise from 'restify-await-promise';
 
 const asyncAwaitMiddleware = (server: Server) => {
-  // The second param takes options using which you can check on every exception 
+  // The second param takes options using which you can check on every exception
   restifyPromise.install(server);
 };
 
